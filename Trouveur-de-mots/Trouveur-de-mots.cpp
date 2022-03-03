b6b99c5133305caa26083c7f28a4918244d022ea
Force-quit <miloulune1@gmail.com>
2022-03-03 14:06:19 -0500

    Trouveur de mots updated with vector

--- Trouveur-de-mots/Trouveur-de-mots.cpp
+++ Trouveur-de-mots/Trouveur-de-mots.cpp
@@ -1,20 +1,16 @@
 #include <fstream>
 #include <string>
 #include <iostream>
-#include <Windows.h>
 #include <iomanip>
 #include <unordered_map>
-#include <memory>
 #include "../Utilities/utils.h"
 
 
-const unsigned int NB_FILE_LINES = 208913;
-
-std::unordered_map<short, std::string> findWord(const std::unique_ptr<std::string[]>& wordList, const std::string& subString)
+std::unordered_map<short, std::string> findWord(const std::vector<std::string>& wordList, const std::string& subString, const unsigned int& NB_WORDS, const unsigned short MAX_FINDS)
 {
-	short counter{};
+	unsigned short counter{};
 	std::unordered_map<short, std::string> matches{};
-	for (unsigned int i = 0; i < NB_FILE_LINES; i++)
+	for (unsigned int i = 0; i < NB_WORDS; i++)
 	{
 		if (wordList[i].find(subString) != std::string::npos)
 		{
@@ -22,11 +18,93 @@ std::unordered_map<short, std::string> findWord(const std::unique_ptr<std::strin
 			std::cout << std::setw(3) << counter << ") " << wordList[i] << "\n";
 			std::pair<short, std::string> mapping(counter, wordList[i]);
 			matches.insert(mapping);
-			if (counter == 25)
+			if (counter == MAX_FINDS)
 				break;
 		}
 	}
 	return matches;
+}
+
+unsigned int fillWordsList(std::vector<std::string>& wordList)
+{
+	std::ifstream file("francais.txt");
+	unsigned int nbLines{ 0 };
+	if (!file.good())
+	{
+		std::cout << "La liste de mots n'est pas à la bonne place...";
+	}
+	else
+	{
+		std::string s;
+		std::cout << "Lecture du fichier de mots...\n";
+		while (std::getline(file, s))
+			++nbLines;
+
+		file.clear();
+		file.seekg(0);
+
+		wordList.resize(nbLines);
+
+		for (unsigned int i = 0; i < nbLines; i++)
+			std::getline(file, wordList[i]);
+
+		file.close();
+		std::cout << "Lecture terminée.\n\n";
+	}
+
+
+	return nbLines;
+}
+
+void mainLoop(std::vector<std::string>& wordList, const unsigned int WORDS_LIST_LENGTH, const unsigned short MAX_FINDS)
+{
+	std::string subString{};
+	std::unordered_map<short, std::string> matchesMap;
+	short choice{};
+
+	bool findOtherWords = true;
+	while (findOtherWords)
+	{
+		std::cout << "-------------------------" << '\n';
+
+		do
+		{
+			emile::flushTampon();
+			std::cout << "Échantillon de mot à trouver : ";
+			std::cin >> subString;
+		} while (std::cin.fail());
+
+
+		for (auto& c : subString)
+			c = std::tolower(c);
+
+		if (subString == "0")
+		{
+			findOtherWords = false;
+			continue;
+		}
+
+		matchesMap = findWord(wordList, subString, WORDS_LIST_LENGTH, MAX_FINDS);
+
+		if (matchesMap.size() == 0)
+			std::cout << "Aucun mot avec cet échantillon\n";
+		else
+		{
+			do
+			{
+				emile::flushTampon();
+				std::cout << "Numéro du mot à copier : ";
+				std::cin >> choice;
+			} while (std::cin.fail() || choice > matchesMap.size() || choice < 0);
+
+			if (choice == 0)
+				continue;
+
+			emile::copyToClipBoard(matchesMap.at(choice));
+			std::cout << "Mot copié dans le clipboard windows\n";
+		}
+	}
+
 }
 
 int main()
@@ -36,54 +114,20 @@ int main()
 	SetConsoleTitleA("Trouveur de mots avec échantillon");
 
 
-	std::ifstream file("francais.txt");
-	if (!file.good())
+	std::vector<std::string> wordList;
+	const unsigned int WORDS_LIST_LENGTH = fillWordsList(wordList);
+
+	if (WORDS_LIST_LENGTH == 0)
 	{
-		std::cout << "Ey! Yé où mon fichier texte?";
-		Sleep(5000);
+		emile::flushTampon();
+		std::cin.get();
 	}
 	else
 	{
-		auto wordList = std::make_unique<std::string[]>(NB_FILE_LINES);
+		const unsigned short MAX_FINDS = 25;
 
-		for (unsigned int i = 0; i < NB_FILE_LINES; i++)
-			std::getline(file, wordList[i]);
-
-		file.close();
-
-		std::string subString{};
-		std::unordered_map<short, std::string> map;
-		short choice{};
 		std::cout << "Trouveur de mots avec échantillon\n";
-		while (true)
-		{
-			std::cout << "-------------------------" << '\n';
-			std::cout << "Échantillon de mot à trouver : ";
-			std::cin >> subString;
-
-			
-			for (auto& c : subString)
-				c = std::tolower(c);
-			
-			map = findWord(wordList, subString);
-			if (map.size() > 0)
-			{
-				do
-				{
-					emile::flushTampon();
-					std::cout << "Numéro du mot à copier : ";
-					std::cin >> choice;
-				} while (std::cin.fail() || choice > map.size() || choice < 0);
-
-				if (choice == 0)
-					continue;
-
-				emile::copyToClipBoard(map.at(choice));
-				std::cout << "Mot copié dans le clipboard windows\n";
-			}
-			else
-				std::cout << "Aucun mot avec cet échantillon\n";
-		}
+		mainLoop(wordList, WORDS_LIST_LENGTH, MAX_FINDS);
 	}
 
 	return 0;
