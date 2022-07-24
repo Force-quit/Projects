0c10f278ac79600139b00c9eb64effa9a027c4ac
Émile Laforce <miloulune1@gmail.com>
2022-07-23 23:03:09 -0400

    Update Qt Autoclicker

--- Qt/Auto/Mouse/AutoClicker/AutoClicker.cpp
+++ Qt/Auto/Mouse/AutoClicker/AutoClicker.cpp
@@ -1,34 +1,87 @@
 #include "AutoClicker.h"
-
-#include <QWidget>
-#include <QHBoxLayout>
 #include <QGroupBox>
-#include <QFormLayout>
-#include <QSlider>
+#include <QHBoxLayout>
 #include <QLabel>
+#include <QPushButton>
+#include <QIntValidator>
+#include <QLineEdit>
+#include <QMessageBox>
 
-AutoClicker::AutoClicker(QWidget *parent)
+
+AutoClicker::AutoClicker(QWidget* parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
 	QWidget* centralWidget{ new QWidget };
 	QHBoxLayout* centralLayout{ new QHBoxLayout };
-	
+
+	QVBoxLayout* leftLayout{ new QVBoxLayout };
+	leftLayout->setAlignment(Qt::AlignTop);
+
 	QGroupBox* parameters{ new QGroupBox("Parameters") };
 	QVBoxLayout* parametersLayout{ new QVBoxLayout };
+
 	QHBoxLayout* clickHoldTimeLayout{ new QHBoxLayout };
-	QSlider* clickHoldTimeSlider{ new QSlider };
-	clickHoldTimeSlider->setOrientation(Qt::Orientation::Horizontal);
-	clickHoldTimeLayout->addWidget(new QLabel("Click hold time :"));
-	clickHoldTimeLayout->addWidget(clickHoldTimeSlider);
+	clickHoldTimeLayout->addWidget(new QLabel("Click hold time (milliseconds):"));
+	int clickHoldTime{};
+	int previousClickHoldTime{};
+	QIntValidator* intValidator{ new QIntValidator };
+	intValidator->setRange(1, INT_MAX);
+	MyLineEdit* clickHoldTimeEdit{ new MyLineEdit };
+	clickHoldTimeEdit->setText(QString::number(50));
+	clickHoldTimeEdit->setValidator(intValidator);
+	connect(clickHoldTimeEdit, &MyLineEdit::focussed, [&previousClickHoldTime, &clickHoldTime, clickHoldTimeEdit, this](bool focussed) {
+		if (focussed)
+		{
+			previousClickHoldTime = clickHoldTimeEdit->text().toInt();
+			clickHoldTimeEdit->setReadOnly(false);
+			// Fucking cursor stays even when out of focus?? So I had to set read only... check this
+		}
+		else
+		{
+			clickHoldTimeEdit->setReadOnly(true);
+
+			clickHoldTime = clickHoldTimeEdit->text().toInt();
+			//QMessageBox::critical(this, tr("Focussed?"), QString(focussed ? "Yes" : "No"), QMessageBox::Ok);
+		}
+	});
+	clickHoldTimeLayout->addWidget(clickHoldTimeEdit);
+
+	QHBoxLayout* timeBetweenClickLayout{ new QHBoxLayout };
+	timeBetweenClickLayout->addWidget(new QLabel("Time between clicks (milliseconds):"));
+	int timeBetweenClick{};
+	QLineEdit* timeBetweenClickEdit{ new QLineEdit };
+	timeBetweenClickEdit->setValidator(intValidator);
+	timeBetweenClickLayout->addWidget(timeBetweenClickEdit);
+
 	parametersLayout->addLayout(clickHoldTimeLayout);
+	parametersLayout->addLayout(timeBetweenClickLayout);
 	parameters->setLayout(parametersLayout);
-	centralLayout->addWidget(parameters);
+	leftLayout->addWidget(parameters);
 
-	QVBoxLayout* rightLayout{ new QVBoxLayout };
-	centralLayout->addLayout(rightLayout);
+	QGroupBox* saveAndLoadGroupBox{ new QGroupBox("Save or load parameters") };
+	QHBoxLayout* saveAndLoadLayout{ new QHBoxLayout };
+	QPushButton* saveButton{ new QPushButton("Save") };
+	QPushButton* loadButton{ new QPushButton("Load") };
+	saveAndLoadLayout->addWidget(saveButton);
+	saveAndLoadLayout->addWidget(loadButton);
+	saveAndLoadGroupBox->setLayout(saveAndLoadLayout);
+	leftLayout->addWidget(saveAndLoadGroupBox);
 
-
+	centralLayout->addLayout(leftLayout);
 	centralWidget->setLayout(centralLayout);
 	setCentralWidget(centralWidget);
+}
+
+
+void MyLineEdit::focusInEvent(QFocusEvent* e)
+{
+	QLineEdit::focusInEvent(e);
+	emit(focussed(true));
+}
+
+void MyLineEdit::focusOutEvent(QFocusEvent* e)
+{
+	QLineEdit::focusInEvent(e);
+	emit(focussed(false));
 }
--- Qt/Auto/Mouse/AutoClicker/AutoClicker.h
+++ Qt/Auto/Mouse/AutoClicker/AutoClicker.h
@@ -2,13 +2,27 @@
 
 #include <QMainWindow>
 #include "ui_AutoClicker.h"
+#include <QLineEdit>
 
 class AutoClicker : public QMainWindow
 {
 	Q_OBJECT
 
 public:
-	AutoClicker(QWidget *parent = nullptr);
+	AutoClicker(QWidget* parent = nullptr);
 private:
 	Ui::AutoClickerClass ui;
+};
+
+class MyLineEdit : public QLineEdit
+{
+	Q_OBJECT
+
+signals:
+	void focussed(bool hasFocus);
+
+
+protected:
+	void focusInEvent(QFocusEvent* e) override;
+	void focusOutEvent(QFocusEvent* e) override;
 };
