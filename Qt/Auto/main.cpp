eee43bb37c706d6f5baa1139cbb71d813bd95926
Force-quit <miloulune1@gmail.com>
2022-11-13 22:37:15 -0500

    Removed beep

--- Qt/Auto/main.cpp
+++ Qt/Auto/main.cpp
@@ -1,10 +1,8 @@
 #include "AutoUtils.h"
 #include <QtWidgets/QApplication>
-#include <Windows.h>
 
 int main(int argc, char *argv[])
 {
-    Beep(160, 2000);
     QApplication a(argc, argv);
     AutoUtils w;
     w.show();
