3f26d4c933ba7033424ff817aab3ab79ee18f3cd
Force-quit <miloulune1@gmail.com>
2023-01-12 13:20:23 -0500

    Starting UI and layout placements

--- Qt/InputRecorder/EQInputRecorder.cpp
+++ Qt/InputRecorder/EQInputRecorder.cpp
@@ -1,21 +1,63 @@
 #include "EQInputRecorder.h"
 #include <QBoxLayout>
 #include <QLabel>
-
-
+#include <QGroupBox>
+#include <QIcon>
 
 EQInputRecorder::EQInputRecorder(QWidget *parent)
-	: QMainWindow(parent)
+	: QMainWindow(parent), currentRecordingText()
 {
-	QVBoxLayout* centralLayout{ new QVBoxLayout };
 	QWidget* centralWidget{ new QWidget };
-	
-	QLabel* test{ new QLabel("Testing") };
-	centralLayout->addWidget(test);
+	QVBoxLayout* centralLayout{ new QVBoxLayout };
 
+	QHBoxLayout* bottomLayout{ new QHBoxLayout };
+	bottomLayout->addWidget(initRecordingGroupBox());
+	bottomLayout->addWidget(initPlayingGroupBox());
+
+	centralLayout->addWidget(initTopGroupBox());
+	centralLayout->addLayout(bottomLayout);
 	centralWidget->setLayout(centralLayout);
 	setCentralWidget(centralWidget);
+	setWindowIcon(QIcon("inputRecorder.png"));
 }
 
-EQInputRecorder::~EQInputRecorder()
-{}
+QGroupBox* EQInputRecorder::initTopGroupBox()
+{
+	QGroupBox* currentRecordingGroupBox{ new QGroupBox("Recording selection") };
+	QHBoxLayout* groupBoxLayout{ new QHBoxLayout };
+	QLabel* currentRecordingLabel{ new QLabel("Current recording :") };
+	currentRecordingText = new QLabel("None");
+
+	groupBoxLayout->addWidget(currentRecordingLabel);
+	groupBoxLayout->addWidget(currentRecordingText);
+	currentRecordingGroupBox->setLayout(groupBoxLayout);
+	return currentRecordingGroupBox;
+}
+
+QGroupBox* EQInputRecorder::initRecordingGroupBox()
+{
+	QGroupBox* recordingGroupBox{ new QGroupBox("Record") };
+	QVBoxLayout* groupBoxLayout{ new QVBoxLayout };
+	QLabel* currentRecordingLabel{ new QLabel("Current recording :") };
+	currentRecordingText = new QLabel("None");
+
+	groupBoxLayout->addWidget(currentRecordingLabel);
+	groupBoxLayout->addWidget(currentRecordingText);
+	recordingGroupBox->setLayout(groupBoxLayout);
+	return recordingGroupBox;
+}
+
+QGroupBox* EQInputRecorder::initPlayingGroupBox()
+{
+	QGroupBox* currentRecordingGroupBox{ new QGroupBox("Playback") };
+	QVBoxLayout* groupBoxLayout{ new QVBoxLayout };
+	QLabel* currentRecordingLabel{ new QLabel("Current recording :") };
+	currentRecordingText = new QLabel("None");
+
+	groupBoxLayout->addWidget(currentRecordingLabel);
+	groupBoxLayout->addWidget(currentRecordingText);
+	currentRecordingGroupBox->setLayout(groupBoxLayout);
+	return currentRecordingGroupBox;
+}
+
+EQInputRecorder::~EQInputRecorder() {}
