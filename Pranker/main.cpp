2cd773a8d3d6978ea4db8528b771b062cee4cd89
Force-quit <miloulune1@gmail.com>
2023-03-14 20:11:58 -0400

    Update pranker

--- Pranker/main.cpp
+++ Pranker/main.cpp
@@ -1,38 +1,90 @@
 #include <Windows.h>
-#include "pranker.h"
 #include "../Utilities/utils.h"
-#include <ctime>
+#include <iostream>
 #include <random>
+
+void prank_CAPSLOCK()
+{
+	if (GetKeyState(VK_CAPITAL) == 0)
+		emile::fullKeyPress(VK_CAPITAL);
+}
+
+void prank_WINDOWS()
+{
+	emile::fullKeyPress(VK_LWIN);
+}
+
+void prank_SPACE()
+{
+	emile::fullKeyPress(VK_SPACE);
+}
+
+void prank_RETURN()
+{
+	emile::fullKeyPress(VK_RETURN);
+}
+
+void prank_BACKSPACE()
+{
+	emile::fullKeyPress(VK_BACK);
+}
+
+void prank_CTRLV()
+{
+	emile::ctrlV();
+}
+
+void prank_ESCAPE()
+{
+	emile::fullKeyPress(VK_ESCAPE);
+}
 
 int main()
 {	
-	if (checkMultipleInstances())
-		return 0;
-	
-	using prank_function_type = void (*)();
 	FreeConsole();
-	srand(time(0));
+	using prank_function_type = void (*)();
 
 	std::vector<prank_function_type> prankFunctions;
-	prankFunctions.push_back(prank_CAPSLOCK);
 	prankFunctions.push_back(prank_WINDOWS);
+	prankFunctions.push_back(prank_CAPSLOCK);
 	prankFunctions.push_back(prank_BACKSPACE);
 	prankFunctions.push_back(prank_CTRLV);
 	prankFunctions.push_back(prank_RETURN);
 	prankFunctions.push_back(prank_SPACE);
 	prankFunctions.push_back(prank_ESCAPE);
+	
+	const int MAX_INDEX{ static_cast<int>(prankFunctions.size() - 1) };
+	const int MIN_INTERVAL{ 30000 };
+	const int MAX_INTERVAL{ 120000 };
 
-	std::clock_t start;
-	start = std::clock();
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> indexDistr(0, MAX_INDEX);
+	std::uniform_int_distribution<> intervalDistr(MIN_INTERVAL, MAX_INTERVAL);
+
 	GetAsyncKeyState(VK_RCONTROL);
-	while (!GetAsyncKeyState(VK_RCONTROL))
+	std::clock_t start{};
+	std::clock_t nextEvent{};
+	bool userQuit{};
+
+	while (!userQuit)
 	{
-		if (std::clock() - start >= rangedRandom(30000, 120000))
+		start = std::clock();
+		nextEvent = intervalDistr(gen);
+
+		while (std::clock() - start < nextEvent)
 		{
-			prankFunctions[rangedRandom(0, prankFunctions.size() - 1)]();
-			start = std::clock();
+			Sleep(5);
+
+			if (GetAsyncKeyState(VK_RCONTROL))
+			{
+				userQuit = true;
+				break;
+			}
 		}
-		Sleep(5);
+
+		if (!userQuit)
+			prankFunctions[indexDistr(gen)]();
 	}
 
 	MessageBoxA(NULL, "haha keyboard go bing bong", "Get pranked", MB_ICONINFORMATION);
