44b3e5aaf39747ca599b3998355ccc97dabbcbde
Force-quit <miloulune1@gmail.com>
2024-01-17 13:13:20 -0500

    Update Pranker NOT READY FOR RELEASE

--- VS/Pranker/Headers/pranker.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-
-void startPranking(const bool& continuePranking);
-bool passwordIsTyped(const std::vector<int>& passwordKeys);
-bool passwordIsTyped(const std::vector<int>& password, const size_t i);
-void waitForKeyRelease(const int key);
-void resetInputBuffer();
--- VS/Pranker/Source/main.cpp
+++ VS/Pranker/Source/main.cpp
@@ -1,6 +1,87 @@
+import EUtilities;
+
 #include <Windows.h>
+#include <random>
+#include <span>
 #include <thread>
-#include "../Headers/pranker.h"
+#include <array>
+
+static void resetInputBuffer()
+{
+	for (int key{ 0x01 }; key <= 0xFE; ++key)
+		GetAsyncKeyState(key);
+}
+
+static bool passwordIsTyped(std::span<int> password, const size_t nextCharIndex)
+{
+	bool wKeyPressed{};
+	bool rightKeyPressed{};
+
+	while (!wKeyPressed)
+	{
+		for (int key{ 0x01 }; key <= 0xFE; ++key)
+		{
+			if (GetAsyncKeyState(key))
+			{
+				wKeyPressed = true;
+				EUtilities::waitForKeyRelease(key);
+				rightKeyPressed = key == password[nextCharIndex];
+				break;
+			}
+		}
+
+		Sleep(5);
+	}
+
+	if (!rightKeyPressed)
+		return false;
+
+	if (password.size() == nextCharIndex + 1)
+		return true;
+
+	return passwordIsTyped(password, nextCharIndex + 1);
+}
+
+static void startPranking(const bool& continuePranking)
+{
+	using prankFunctionType = void (*)();
+	std::vector<prankFunctionType> prankFunctions{
+		[]() {EUtilities::fullKeyPress(VK_LWIN); },
+		[]() {EUtilities::fullKeyPress(VK_CAPITAL); },
+
+		/* Dangerous functions
+		[]() {EUtilities::fullKeyPress(VK_SPACE); },
+		[]() {EUtilities::fullKeyPress(VK_RETURN); },
+		[]() {EUtilities::fullKeyPress(VK_BACK); },
+		[]() {EUtilities::ctrlV(); },
+		[]() {EUtilities::fullKeyPress(VK_ESCAPE); }
+		*/
+	};
+
+	const int MAX_INDEX{ static_cast<int>(prankFunctions.size() - 1) };
+	static constexpr int MIN_INTERVAL{ 15000 };
+	static constexpr int MAX_INTERVAL{ 30000 };
+
+	std::random_device randomDevice;
+	std::mt19937 randomGenerator(randomDevice());
+	std::uniform_int_distribution<> indexDistribution(0, MAX_INDEX);
+	std::uniform_int_distribution<> intervalDistribution(MIN_INTERVAL, MAX_INTERVAL);
+
+	std::clock_t lastPrankTime{};
+	std::clock_t randomInterval{};
+
+	while (continuePranking)
+	{
+		lastPrankTime = std::clock();
+		randomInterval = intervalDistribution(randomGenerator);
+
+		while (std::clock() - lastPrankTime < randomInterval && continuePranking)
+			Sleep(5);
+
+		if (continuePranking)
+			prankFunctions[indexDistribution(randomGenerator)]();
+	}
+}
 
 int WinMain(
 	_In_ HINSTANCE hInstance,
@@ -12,8 +93,8 @@ int WinMain(
 	std::thread prankThread(startPranking, std::cref(wContinuePranking));
 
 	// https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
-	// Emile le hacker
-	const std::vector<int> passwordKeys{
+	// emile le hacker
+	std::array<int, 15> passwordKeys{
 		0x45, 0x4D, 0x49, 0x4C, 0x45,
 		VK_SPACE,
 		0x4C, 0x45,
@@ -24,7 +105,7 @@ int WinMain(
 	resetInputBuffer();
 
 	while (wContinuePranking)
-		wContinuePranking = !passwordIsTyped(passwordKeys);
+		wContinuePranking = !passwordIsTyped(passwordKeys, 0);
 
 	prankThread.join();
 	MessageBoxA(NULL, "haha keyboard go bing bong", "Get pranked", MB_ICONINFORMATION | MB_SYSTEMMODAL);
