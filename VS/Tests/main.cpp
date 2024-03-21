97e40e00ed568a7eaf9945a5af68f0731c0d85cc
Force-quit <miloulune1@gmail.com>
2024-03-21 12:24:46 -0400

    Updated Test project

--- VS/Tests/main.cpp
+++ VS/Tests/main.cpp
@@ -1,17 +1,12 @@
 ﻿import eutilities;
 import EShortcutListener;
-#include <vector>
+
 #include <iostream>
-#include <functional>
+
 
 int main()
 {
-	std::vector<eutilities::Key> a =
-	{
-		eutilities::Key::CAPSLOCK,
-	};
-
-	EShortcutListener::setTargetKeys(a);
-	EShortcutListener::startListening([]{eutilities::fullKeyPress(eutilities::WINDOWS); });
+	
+	
 	std::cin.get();
 }
